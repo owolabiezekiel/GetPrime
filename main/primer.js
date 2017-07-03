5cc84a85f823be943ea86b33dbf8224081ce2fa4
= <owo.ezekiel@gmail.com>
2017-07-03 15:19:24 +0100

    added comments to primer.js

--- main/primer.js
+++ main/primer.js
@@ -2,13 +2,21 @@
 
 module.exports = {
     getPrimes: (n) => {
+        // create an array to hold the prime numbers if any
         let primes = [];
+
+        // this code handles input that is not a number
         if (typeof(n) != 'number') {
             return "undefined";
-        } else if (n <= 1) {
+        }
+        // this code checks if input value is less than or equal to 1. it returns immediately
+        else if (n <= 1) {
             return "undefined";
-        } else {
+        }
+        // if the input value is greater than or equal to 2
+        else {
             for (var i = 2; i <= n; i++) {
+                //calls the isPrime function on the value of iterator
                 if (isPrime(i))
                     primes.push(i);
 
@@ -16,6 +24,7 @@ module.exports = {
             return primes;
         }
 
+        // function to check if a value is prime
         function isPrime(num) {
             if (num < 2) return false;
             for (let i = 2; i < num; i++) {
